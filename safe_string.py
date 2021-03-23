d6b0195bcd864d5b17f978c2f4413be3ce5465be
agnes-sharan <agnessharan@gmail.com>
2021-03-23 13:11:35 -0400

    Add asr string functions

--- safe_string.py
+++ safe_string.py
@@ -14,11 +14,20 @@ class safe_string:
     def __eq__(self, other):
         return self.string.__eq__(other.string)
 
+    def __ne__(self, other):
+        if isinstance(other, safe_string):
+            return self.string.__ne__(other.string)
+        else:
+            return self.string.__ne__(other)
+
     def __ge__(self, other):
         return self.string.__ge__(other.string)
 
     def __contains__(self, subs):
         return self.string.__contains__(subs.string)
+
+    def __new__(cls, string, trusted=None):
+        return super(safe_string, cls).__new__(cls)
 
     def title(self):
         return safe_string(
@@ -132,6 +141,72 @@ class safe_string:
 
         return final
 
+    def split(self, sep, maxsplit=math.inf):
+        str_splits = self.string.split(sep, min(maxsplit, len(self.string)))
+        start_idx = 0
+        list_splits = []
+        for ele in str_splits:
+            new_ele = safe_string(ele, trusted=self.trusted[start_idx:start_idx+len(ele)])
+            list_splits.append(new_ele)
+            start_idx += len(ele)+1
+        return list_splits
+
+    def capitalize(self):
+        return safe_string(self.string.capitalize(), trusted=self.trusted)
+
+    def center(self, width, fillchar=' '):
+        new_str = self.string.center(width, fillchar)
+        left_fill = int(max(0, (width-len(self.string)+1)/2))*[False]
+        right_fill = int(max(0, (width-len(self.string))/2))*[False]
+        new_trusted = left_fill+self.trusted+right_fill
+        return safe_string(new_str, trusted=new_trusted)
+
+    def find(self, sub, *args):
+        return self.string.find(sub, *args)
+
+    def ljust(self, width, fillchar=' '):
+        new_str = self.string.ljust(width, fillchar)
+        new_trusted = self.trusted + max(0, (width-len(self.string)))*[False]
+        return safe_string(new_str, trusted=new_trusted)
+
+    def rfind(self, sub, *args):
+        return self.string.rfind(sub, *args)
+
+    def rstrip(self, chars=" "):
+        new_str = self.string.rstrip(chars)
+        return safe_string(
+            new_str, 
+            trusted=self.trusted[:len(new_str)])
+
+    def strip(self, chars=" "):
+        new_str = self.string.strip(chars)
+        start_idx = 0
+        while start_idx < len(self.string):
+            if self.string[start_idx] not in chars:
+                break
+            start_idx += 1
+        end_idx = len(self.string)-1
+        while end_idx >= 0:
+            if self.string[end_idx] not in chars:
+                break
+            end_idx -= 1
+        return safe_string(
+            new_str,
+            trusted=self.trusted[start_idx:end_idx+1]
+        )
+    
+    def upper(self):
+        return safe_string(self.string.upper(), trusted=self.trusted)
+
+    def islower(self):
+        return self.string.islower()
+
+    def isspace(self):
+        return self.string.isspace()
+
+    def isidentifier(self):
+        return self.string.isidentifier()
+
 if __name__ == "__main__":
     def test(haystack, *args, **kwargs):
         s = safe_string(haystack, trusted=[True for _ in haystack])
@@ -139,17 +214,17 @@ if __name__ == "__main__":
         print(replaced.string)
         print("".join(str(int(trust)) for trust in replaced.trusted))
 
-    test("foobarblahbarbaz")
-    test("foobarblahbarbaz", count=1)
-    test("foobarblahbar")
-    test("foobarblahbar", count=1)
-    test("foobarblahbar", count=5)
-    test("fooblah", count=5)
+    # test("foobarblahbarbaz")
+    # test("foobarblahbarbaz", count=1)
+    # test("foobarblahbar")
+    # test("foobarblahbar", count=1)
+    # test("foobarblahbar", count=5)
+    # test("fooblah", count=5)
 
-    print()
-    print()
-    print()
-    print()
+    # print()
+    # print()
+    # print()
+    # print()
 
     def test3(haystack, *args, **kwargs):
         s = safe_string(haystack, trusted=[True for _ in haystack])
@@ -180,13 +255,75 @@ if __name__ == "__main__":
         [print(l.string) for l in lines]
  
 
-    test2("foobarblahbarbaz")
-    test2("foobarblahbarbaz", count=1)
-    test2("foobarblahbar")
-    test2("foobarblahbar", count=1)
-    test2("foobarblahbar", count=5)
-    test2("fooblah", count=5)
-    test3("fooblah", count=5)
-    test4("needle in a haystack", "needle")
-    test5("needle", 10, 'a')
-    test6("happy\nbirth\nday")
+    # test2("foobarblahbarbaz")
+    # test2("foobarblahbarbaz", count=1)
+    # test2("foobarblahbar")
+    # test2("foobarblahbar", count=1)
+    # test2("foobarblahbar", count=5)
+    # test2("fooblah", count=5)
+    # test3("fooblah", count=5)
+    # test4("needle in a haystack", "needle")
+    # test5("needle", 10, 'a')
+    # test6("happy\nbirth\nday")
+
+    def center_test(string, trusted, width, fillchar=' '):
+        safe_str = safe_string(string, trusted=trusted)
+        new_safe_str = safe_str.center(width, fillchar)
+        print(new_safe_str.string, len(new_safe_str.string))
+        print(new_safe_str.trusted)
+
+    # center_test("tell", [True]*4, 5)
+    # center_test("tell", [True]*4, 3)
+    # center_test("tell", [True]*4, 6)
+    # center_test("tell", [True,False,True,False], 6)
+    # center_test("tell", [True,False,True,False], 7)
+    # center_test("tell", [True,False,True,False], 2)
+
+    def find_test(string, sub, *args):
+        safe_str = safe_string(string, trusted=len(string)*[True])
+        find = safe_str.find(sub, *args)
+        print(find)
+
+    # find_test("hello", 'h')
+    # find_test("hello", 'h', 2)
+    # find_test("hello", 'e', 1)
+    # find_test("hello", 'l', 2, 3)
+
+    def ljust_test(string, width, fillchar=' '):
+        safe_str = safe_string(string, trusted=len(string)*[True])
+        ljust = safe_str.ljust(width, fillchar)
+        print(ljust.string, len(ljust.string))
+        print(ljust.trusted)
+
+    # ljust_test("hello", 2)
+    # ljust_test("hello", 7)
+    # ljust_test("hello", 8, 'f')
+
+    def rfind_test(string, sub, *args):
+        safe_str = safe_string(string, trusted=len(string)*[True])
+        rfind = safe_str.rfind(sub, *args)
+        print(rfind)
+
+    # rfind_test("hello", 'l', 2, 4)
+
+    def rstrip_test(string, chars=" "):
+        safe_str = safe_string(string, trusted=len(string)*[True])
+        rstrip = safe_str.rstrip(chars)
+        print(rstrip.string, len(rstrip.string))
+        print(rstrip.trusted)
+
+    # rstrip_test("hello   ")
+    # rstrip_test("hello   ", "o")
+    # rstrip_test("hello", "o")
+    # rstrip_test("hell", "o")
+
+    def strip_test(string, chars=" "):
+        safe_str = safe_string(string, trusted=len(string)*[True])
+        strip = safe_str.strip(chars)
+        print(strip.string, len(strip.string))
+        print(strip.trusted)
+
+    # strip_test("hello   ")
+    # strip_test("   he  llo   ")
+    # strip_test("hello   ", "o")
+    # strip_test("hell", "o")
