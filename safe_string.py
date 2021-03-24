c2378b3e7eb46ae30d48c570a54f7bb51ed09fd0
disconsis <ketan.kanishka@nyu.edu>
2021-03-25 00:20:44 +0530

    bug fixes and reformattings

--- safe_string.py
+++ safe_string.py
@@ -1,10 +1,13 @@
 import math
 
 class safe_string:
+    def __new__(cls, string, trusted=None):
+        return super(safe_string, cls).__new__(cls)
+
     def __init__(self, string, trusted=None):
         self.string = string
         self.trusted = (
-            trusted if trusted is not None 
+            trusted if trusted is not None
             else [False for _ in self.string]
         )
 
@@ -35,20 +38,17 @@ class safe_string:
     def __contains__(self, subs):
         return self.string.__contains__(subs.string)
 
-    def __new__(cls, string, trusted=None):
-        return super(safe_string, cls).__new__(cls)
-
     def title(self):
         return safe_string(
-            self.string.title(), 
-            trusted=self.trusted )
-        
+            self.string.title(),
+            trusted=self.trusted)
+
     def isascii(self):
         return self.string.isascii()
 
     def istitle(self):
         return self.string.istitle()
-    
+
     def isdigit(self):
         return self.string.isdigit()
 
@@ -61,7 +61,7 @@ class safe_string:
     def rindex(self, subs, *args, **kwargs):
         return self.string.index(subs.string, *args, **kwargs)
 
-    def expandtabs(self, tabsize = 8):
+    def expandtabs(self, tabsize=8):
         string = ""
         trusted = []
         for i in range(len(self.string)):
@@ -70,17 +70,40 @@ class safe_string:
                 trusted += [self.trusted[i]] * tabsize
             else:
                 string += self.string[i]
-                trusted += [trusted[i]] 
+                trusted += [self.trusted[i]]
         return safe_string(
-            string, 
-            trusted=trusted )
+            string,
+            trusted=trusted)
 
-    def lstrip(self, chars=' '):
-        string = self.string.lstrip(chars=chars)
+    def lstrip(self, chars=None):
+        string = self.string.lstrip(chars)
         return safe_string(
-            string, 
+            string,
             trusted=self.trusted[-len(string):])
 
+    def rstrip(self, chars=None):
+        new_str = self.string.rstrip(chars)
+        return safe_string(
+            new_str,
+            trusted=self.trusted[:len(new_str)])
+
+    def strip(self, chars=None):
+        return self.lstrip(chars).rstrip(chars)
+
+    # TODO cleanup: fillchar is always safe_string
+    def ljust(self, width, fillchar=' '):
+        if isinstance(fillchar, safe_string):
+            char_trust = fillchar.trusted[0]
+            string = self.string.ljust(width, fillchar.string)
+        else:
+            char_trust = False
+            string = self.string.ljust(width, fillchar)
+        trusted = self.trusted + max(0, (width-len(self.string)))*[char_trust]
+        return safe_string(
+            string,
+            trusted=trusted)
+
+    # TODO cleanup: fillchar is always safe_string
     def rjust(self, width, fillchar=' '):
         if isinstance(fillchar, safe_string):
             char_trust = fillchar.trusted[0]
@@ -90,14 +113,14 @@ class safe_string:
             string = self.string.rjust(width, fillchar)
         trusted = self.trusted if ((len(string)-len(self.string)) <= 0) else [char_trust]*(len(string)-len(self.string)) + self.trusted
         return safe_string(
-            string, 
+            string,
             trusted=trusted)
 
     def zfill(self, width):
         string = self.string.zfill(width)
         trusted = self.trusted if ((len(string)-len(self.string)) <= 0) else [False]*(len(string)-len(self.string)) + self.trusted
         return safe_string(
-            string, 
+            string,
             trusted=trusted)
 
     def splitlines(self, keepends=False):
@@ -105,10 +128,13 @@ class safe_string:
         start = 0
         for i in range(len(self.string)):
             if self.string[i] == '\n':
-                safe_string_list += [self[start:i+1]] if keepends else [self[start:i]]
+                safe_string_list.append(self[start:i+1]
+                                        if keepends
+                                        else self[start:i])
                 start = i+1
-                
-        safe_string_list += [self[start:]]
+
+        if start < len(self.string):
+            safe_string_list.append(self[start:])
         return safe_string_list
 
     def startswith(self, prefix, *args, **kwargs):
@@ -126,14 +152,15 @@ class safe_string:
             raise TypeError("Invalid indexing")
 
     def __iter__(self):
-        return self.string.__iter__()
-    
+        for char, trust in zip(self.string, self.trusted):
+            yield safe_string(char, trusted=[trust])
+
     def __len__(self):
         return self.string.__len__()
 
     def __add__(self, other):
         return safe_string(
-            self.string + other.string, 
+            self.string + other.string,
             trusted=self.trusted + other.trusted)
 
     def __mul__(self, num):
@@ -147,9 +174,7 @@ class safe_string:
 
     def replace(self, old, new, count=math.inf):
         final = safe_string("")
-
         start_idx = 0
-
         while count > 0:
             idx = self.string.find(old.string, start_idx)
             if idx == -1:
@@ -160,27 +185,41 @@ class safe_string:
                 count -= 1
 
         final += self[start_idx:]
-
         return final
 
-    def split(self, sep=None, maxsplit=math.inf):
-        str_splits = self.string.split(sep, min(maxsplit, len(self.string)))
+    def split(self, sep=None, maxsplit=-1):
+        str_splits = self.string.split(sep=sep, maxsplit=maxsplit)
+        result = []
+
         start_idx = 0
-        list_splits = []
-        for ele in str_splits:
-            while sep == None and self.string[start_idx:start_idx+len(ele)] != ele:
+        if sep is None:
+            while start_idx < len(self.string) \
+                    and self.string[start_idx].isspace():
                 start_idx += 1
-            new_ele = safe_string(ele, trusted=self.trusted[start_idx:start_idx+len(ele)])
-            list_splits.append(new_ele)
-            start_idx += len(ele)
-            if sep != None and len(sep) > 0:
+
+        for elem in str_splits:
+            elem_trusted = self.trusted[start_idx:start_idx + len(elem)]
+            result.append(safe_string(elem, trusted=elem_trusted))
+
+            start_idx += len(elem)
+            if sep is None:
+                while start_idx < len(self.string) \
+                        and self.string[start_idx].isspace():
+                    start_idx += 1
+            else:
                 start_idx += len(sep)
-        return list_splits
+
+        return result
 
     def rsplit(self, sep=None, maxsplit=-1):
         str_splits = self.string.rsplit(sep=sep, maxsplit=maxsplit)
-        start_idx = 0
         result = []
+
+        start_idx = 0
+        if sep is None:
+            while start_idx < len(self.string) \
+                    and self.string[start_idx].isspace():
+                start_idx += 1
 
         for elem in str_splits:
             elem_trusted = self.trusted[start_idx:start_idx + len(elem)]
@@ -199,61 +238,32 @@ class safe_string:
     def capitalize(self):
         return safe_string(self.string.capitalize(), trusted=self.trusted)
 
+    # TODO cleanup: fillchar is always safe_string
     def center(self, width, fillchar=' '):
+        if len(self) >= width:
+            return self
+
         if isinstance(fillchar, safe_string):
             char_trust = fillchar.trusted[0]
             string = self.string.center(width, fillchar.string)
         else:
             char_trust = False
             string = self.string.center(width, fillchar)
-        left_fill = int(max(0, (width-len(self.string)+1)/2))*[char_trust]
-        right_fill = int(max(0, (width-len(self.string))/2))*[char_trust]
-        trusted = left_fill+self.trusted+right_fill
+
+        marg = width - len(self)
+        left_fill = (marg // 2 + (marg & width & 1)) * [char_trust]
+        right_fill = (marg - len(left_fill)) * [char_trust]
+        trusted = left_fill + self.trusted + right_fill
         return safe_string(
-            string, 
+            string,
             trusted=trusted)
 
     def find(self, sub, *args):
         return self.string.find(sub, *args)
 
-    def ljust(self, width, fillchar=' '):
-        if isinstance(fillchar, safe_string):
-            char_trust = fillchar.trusted[0]
-            string = self.string.ljust(width, fillchar.string)
-        else:
-            char_trust = False
-            string = self.string.ljust(width, fillchar)
-        trusted = self.trusted + max(0, (width-len(self.string)))*[char_trust]
-        return safe_string(
-            string, 
-            trusted=trusted)
-
     def rfind(self, sub, *args):
         return self.string.rfind(sub, *args)
 
-    def rstrip(self, chars=" "):
-        new_str = self.string.rstrip(chars)
-        return safe_string(
-            new_str, 
-            trusted=self.trusted[:len(new_str)])
-
-    def strip(self, chars=" "):
-        new_str = self.string.strip(chars)
-        start_idx = 0
-        while start_idx < len(self.string):
-            if self.string[start_idx] not in chars:
-                break
-            start_idx += 1
-        end_idx = len(self.string)-1
-        while end_idx >= 0:
-            if self.string[end_idx] not in chars:
-                break
-            end_idx -= 1
-        return safe_string(
-            new_str,
-            trusted=self.trusted[start_idx:end_idx+1]
-        )
-    
     def upper(self):
         return safe_string(self.string.upper(), trusted=self.trusted)
 
@@ -287,8 +297,12 @@ class safe_string:
     def isprintable(self):
         return self.string.isprintable()
 
-    def __str__(self):
-        return self.string.__str__()
+    # def __str__(self):
+    #     return self
+
+    # def __repr__(self):
+    #     return safe_string(repr(self.string),
+    #                        trusted=[False] + self.trusted + [False])
 
     # TODO: maybe return a safe_bytestring later
     def encode(self, *args, **kwargs):
@@ -301,7 +315,11 @@ class safe_string:
         return self.string.count(sub.string, *args, **kwargs)
 
     def partition(self, sep):
-        (before, sep_part, after) = self.string.partition(sep)
+        if isinstance(sep, safe_string):
+            (before, sep_part, after) = self.string.partition(sep.string)
+        else:
+            (before, sep_part, after) = self.string.partition(sep)
+
         before_trusted = self.trusted[:len(before)]
         sep_trusted = self.trusted[len(before):len(before) + len(sep_part)]
         after_trusted = self.trusted[len(before) + len(sep_part):]
@@ -312,7 +330,11 @@ class safe_string:
         )
 
     def rpartition(self, sep):
-        (before, sep_part, after) = self.string.rpartition(sep)
+        if isinstance(sep, safe_string):
+            (before, sep_part, after) = self.string.rpartition(sep.string)
+        else:
+            (before, sep_part, after) = self.string.rpartition(sep)
+
         before_trusted = self.trusted[:len(before)]
         sep_trusted = self.trusted[len(before):len(before) + len(sep_part)]
         after_trusted = self.trusted[len(before) + len(sep_part):]
@@ -433,8 +455,12 @@ if __name__ == "__main__":
     # rstrip_test("hello", "o")
     # rstrip_test("hell", "o")
 
-    def strip_test(string, chars=" "):
-        safe_str = safe_string(string, trusted=len(string)*[True])
+    def dbg_safestring(string):
+        return safe_string(string, [True, False] * (len(string) // 2) + [True] * (len(string) % 2))
+
+
+    def strip_test(string, chars=None):
+        safe_str = dbg_safestring(string)
         strip = safe_str.strip(chars)
         print(strip.string, len(strip.string))
         print(strip.trusted)
@@ -462,3 +488,28 @@ if __name__ == "__main__":
     # rsplit_test("-abc--d-ef-", sep="-", maxsplit=3)
     # rsplit_test("-abc--d-ef-", sep="--")
     # rsplit_test("-abc--d-ef-", sep="--", maxsplit=3)
+
+    def lstrip_test(string, chars=" "):
+        safe_str = safe_string(string, trusted=(len(string) // 2)*[True, False]
+                               + [True] * (len(string) % 2))
+        strip = safe_str.lstrip(chars)
+        print(strip.string, len(strip.string))
+        print(strip.trusted)
+
+    # lstrip_test("hello   ")
+    # lstrip_test("   he  llo   ")
+    # lstrip_test("  he  llo   ")
+    # lstrip_test("hello   ", "o")
+    # lstrip_test("hell", "o")
+
+    def split_test(string, *args, **kwargs):
+        safestring = safe_string(string,
+                                 [True, False] * (len(string) // 2) + [True] * (len(string) % 2))
+        print([(elem.string, elem.trusted) for elem in safestring.split(*args,
+                                                                        **kwargs)])
+
+    def rsplit_test(string, *args, **kwargs):
+        safestring = safe_string(string,
+                                 [True, False] * (len(string) // 2) + [True] * (len(string) % 2))
+        print([(elem.string, elem.trusted) for elem in safestring.rsplit(*args,
+                                                                         **kwargs)])
