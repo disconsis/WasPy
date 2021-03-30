7a9b0073dbf405f645bc62cc4595d610278e196a
disconsis <ketan.kanishka@nyu.edu>
2021-03-30 14:25:43 +0530

    reformat

--- format.py
+++ format.py
@@ -1,38 +1,38 @@
 def _do_build_string(s):
-    isHole = False 
+    isHole = False
     res = []
     holes = []
-    lb_loc = 0 
+    lb_loc = 0
     i = 0
 
     while i < len(s):
         if s[i] == '{':
             if i < len(s)-1 and s[i+1] == '{':
-                res.append(('l', (i,i+1)))
-                isHole = False 
-                i+=1
-            else: 
-                isHole = True 
-                lb_loc = i
-        
-        elif s[i] == '}':
-            if isHole: 
-                res.append(('h', (lb_loc,i)))
-                holes.append((lb_loc,i))
+                res.append(('l', (i, i+1)))
                 isHole = False
-            elif i < len(s)-1 and s[i+1] == '}': 
-                res.append(('r', (i,i+1)))
-                i+=1
-        i+=1
-                
+                i += 1
+            else:
+                isHole = True
+                lb_loc = i
 
-    print(holes)          
-    print(res)  
+        elif s[i] == '}':
+            if isHole:
+                res.append(('h', (lb_loc, i)))
+                holes.append((lb_loc, i))
+                isHole = False
+            elif i < len(s)-1 and s[i+1] == '}':
+                res.append(('r', (i, i+1)))
+                i += 1
+        i += 1
+
+    print(holes)
+    print(res)
     print()
 
 
 def KETAN(hole):
-    return a, b,c
+    return a, b, c
+
 
 def render_field(holes):
     result = []
@@ -41,17 +41,20 @@ def render_field(holes):
         s = value
         is_safe = False
         if isinstance(value, safe_string):
-            is_safe = True 
+            is_safe = True
             s = value.string
 
     if conv == 'r':
-        trusted = value.__repr__().trusted if is_safe else [False]*len(repr(s).__format__(spec))
+        trusted = value.__repr__().trusted if is_safe else [
+            False]*len(repr(s).__format__(spec))
         result.append(trusted)
     elif conv == 's':
-        trusted = value.__str__().trusted if is_safe else [False]*len(str(s).__format__(spec))
+        trusted = value.__str__().trusted if is_safe else [
+            False]*len(str(s).__format__(spec))
         result.append(trusted)
     elif conv == 'a':
-        trusted = [False] + value.trusted + [False] if is_safe else [False]*(len(value.string)+2)
+        trusted = [False] + value.trusted + \
+            [False] if is_safe else [False]*(len(value.string)+2)
         result.append(trusted)
     else:
         trusted = value.trusted if is_safe else [False]*len(s.__format__(spec))
@@ -60,7 +63,7 @@ def render_field(holes):
     return result
 
 
-def construct_trusted(format_string, gl_holes, trusted_result): 
+def construct_trusted(format_string, gl_holes, trusted_result):
     prev_index = 0
     gl_index = 0
     hl_index = 0
@@ -87,5 +90,5 @@ def construct_trusted(format_string, gl_holes, trusted_result):
 
 
 test_cases = ["{abc}", "{{", "}}", "{{{}}}", "{}{}{}", "}", "{{{{}}}}"]
-for s in test_cases: 
+for s in test_cases:
     _do_build_string(s)
