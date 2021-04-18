3a195671f42239c89d68c73c29b18b39df70f7d0
disconsis <ketan.kanishka@nyu.edu>
2021-04-18 20:56:31 +0530

    move str.format code to safe_string file to stop circular import

--- safe_string/safe_string.py
+++ safe_string/safe_string.py
@@ -1,6 +1,274 @@
 import math
 
 
+class OverrideUnsafe:
+    def __init__(self, safe_obj):
+        self.obj = safe_obj
+
+    def __format__(self, *args, **kwargs):
+        if isinstance(self.obj, safe_string):
+            result = safe_format(self.obj, *args, **kwargs)
+        else:
+            result = self.obj.__format__(*args, **kwargs)
+
+        if isinstance(result, safe_string):
+            return result.string
+        else:
+            return result
+
+    def __repr__(self):
+        result = self.obj.__repr__()
+        if isinstance(result, safe_string):
+            return result.string
+        else:
+            return result
+
+    def __str__(self):
+        result = self.obj.__str__()
+        if isinstance(result, safe_string):
+            return result.string
+        else:
+            return result
+
+    def __getitem__(self, *args, **kwargs):
+        return OverrideUnsafe(self.obj.__getitem__(*args, **kwargs))
+
+    def __getattr__(self, *args, **kwargs):
+        return OverrideUnsafe(self.obj.__getattribute__(*args, **kwargs))
+
+
+def safe_format(fmt_string, *args, **kwargs):
+    if not isinstance(fmt_string, safe_string):
+        fmt_string = safe_string(fmt_string)
+    unsafe_args = list(map(OverrideUnsafe, args))
+    unsafe_kwargs = {key: OverrideUnsafe(value)
+                     for key, value in kwargs.items()}
+
+    result_string = fmt_string.string.format(*unsafe_args, **unsafe_kwargs)
+    arg_holes, all_holes = _do_build_string(fmt_string)
+    arg_hole_trusts = render_field(arg_holes, fmt_string, args, kwargs)
+    final_trusted = construct_trusted(fmt_string, all_holes, arg_hole_trusts)
+    if len(result_string) != len(final_trusted):
+        print("ERROR:")
+        dbg_print_safestring(safe_string(result_string, trusted=final_trusted))
+        print(
+            f"str len = {len(result_string)} | trust len = {len(final_trusted)}")
+        raise Exception("mismatched lengths")
+    return safe_string(result_string, trusted=final_trusted)
+
+
+def _do_build_string(s):
+    isHole = False
+    res = []
+    holes = []
+    lb_loc = 0
+    i = 0
+
+    while i < len(s):
+        if s[i] == '{':
+            if i < len(s)-1 and s[i+1] == '{':
+                res.append(('l', (i, i+1)))
+                isHole = False
+                i += 1
+            else:
+                isHole = True
+                lb_loc = i
+        elif s[i] == '[':
+            while s[i] != ']':
+                i += 1
+        elif s[i] == '}':
+            if isHole:
+                res.append(('h', (lb_loc, i)))
+                holes.append((lb_loc, i))
+                isHole = False
+            elif i < len(s)-1 and s[i+1] == '}':
+                res.append(('r', (i, i+1)))
+                i += 1
+        i += 1
+
+    return holes, res
+
+
+def render_field(holes, fmt_string, args, kwargs):
+    result = []
+    parser = field_parser(args, kwargs)
+    for hole in holes:
+        value, conv, spec = parser(fmt_string[hole[0]:hole[1] + 1])
+        if not isinstance(spec, safe_string):
+            spec = safe_string(spec)
+        s = value
+        is_safe = False
+        if isinstance(value, safe_string):
+            is_safe = True
+            s = value.string
+
+        if conv == 'r':
+            trusted = value.__repr__().trusted if is_safe else [
+                False]*len(repr(s).__format__(spec.string))
+            result.append(trusted)
+        elif conv == 's':
+            trusted = value.__str__().trusted if is_safe else [
+                False]*len(str(s).__format__(spec.string))
+            result.append(trusted)
+        elif conv == 'a':
+            if not is_safe:
+                trusted = [False] * len(ascii(value))
+            elif value.isascii():
+                trusted = [False] + value.trusted + [False]
+            else:
+                # go char-by-char and call ascii
+                trusted = []
+                for char in value:
+                    newchar = ascii(char.string)[1:-1]  # remove quotes
+                    trusted += len(newchar) * char.trusted
+                trusted = [False] + trusted + [False]  # for quotes
+
+            result.append(trusted)
+        else:
+            trusted = value.trusted if is_safe else [
+                False]*len(s.__format__(spec.string))
+            result.append(trusted)
+
+    return result
+
+
+def construct_trusted(format_string, gl_holes, trusted_result):
+    prev_index = 0
+    gl_index = 0
+    hl_index = 0
+    final_trusted = []
+    while gl_index < len(gl_holes):
+        start = gl_holes[gl_index][1][0]
+        end = gl_holes[gl_index][1][1]
+        final_trusted += format_string.trusted[prev_index:start]
+        new_trusted = []
+        if gl_holes[gl_index][0] == 'l' or gl_holes[gl_index][0] == 'r':
+            new_trusted += [format_string.trusted[end]]
+        else:
+            new_trusted += trusted_result[hl_index]
+            hl_index += 1
+        final_trusted += new_trusted
+        gl_index += 1
+        prev_index = end+1
+
+    final_trusted += format_string.trusted[prev_index:]
+
+    return final_trusted
+
+
+test_cases = ["{abc}", "{{", "}}", "{{{}}}", "{}{}{}", "}", "{{{{}}}}"]
+for s in test_cases:
+    _do_build_string(s)
+
+
+def parse_field(hole):
+    hole = hole[1:-1]
+
+    end = len(hole)
+    i = 0
+    while i < end:
+        c = hole[i]
+        if c == "!":
+            # <name>  !  <conv>   :   <spec>
+            #         i   i+1    i+2  i+3...
+            name = hole[:i]
+            conversion = hole[i + 1]
+            if i + 2 < end:
+                # then :<spec> exists
+                spec = hole[i+3:]
+            else:
+                spec = safe_string("")
+
+            return name, conversion, spec
+
+        elif c == ":":
+            # <name>  :  <spec>
+            #         i  i+1...
+            name = hole[:i]
+            spec = hole[i+1:]
+
+            return name, None, spec
+
+        elif c == "[":
+            while hole[i] != "]":
+                i += 1
+
+        i += 1
+
+    # no : or ! encountered
+    return hole, None, ""
+
+
+def get_argument(args, kwargs):
+    auto_numbering = -1
+
+    def _get_argument(name):
+        nonlocal auto_numbering
+
+        i = 0
+        end = len(name)
+        while i < end and name[i] not in ("[", "."):
+            i += 1
+
+        index = name[:i]
+        if not index:
+            auto_numbering += 1
+            return args[auto_numbering]
+        elif index.isnumeric():
+            return args[int(index)]
+        else:
+            return kwargs[index]
+
+    return _get_argument
+
+
+def resolve_lookups(obj, name):
+    end = len(name)
+
+    i = 0
+    while i < end and name[i] not in ("[", "."):
+        i += 1
+
+    while i < end:
+        c = name[i]
+        if c == ".":
+            i += 1
+            start = i
+            while i < end:
+                if name[i] in (".", "["):
+                    break
+                i += 1
+            attr = name[start:i]
+            obj = getattr(obj, attr)
+
+        elif c == "[":
+            i += 1
+            start = i
+            while i < end:
+                if name[i] == "]":
+                    break
+                i += 1
+            index = name[start:i]
+            i += 1  # skip the ']'
+            if index.isnumeric():
+                index = int(index)
+            obj = obj[index]
+
+    return obj
+
+
+def field_parser(args, kwargs):
+    get_arg = get_argument(args, kwargs)
+
+    def _parser(hole):
+        name, conv, spec = parse_field(hole)
+        obj = get_arg(name)
+        obj = resolve_lookups(obj, name)
+        return obj, conv, spec
+
+    return _parser
+
+
 class safe_string:
     def __new__(cls, string, trusted=None):
         return super(safe_string, cls).__new__(cls)
@@ -372,6 +640,12 @@ class safe_string:
             safe_string(sep_part, trusted=sep_trusted),
             safe_string(after, trusted=after_trusted)
         )
+
+    def format(self, *args, **kwargs):
+        return safe_format(self, *args, **kwargs)
+
+    def format_map(self, mapping):
+        return self.format(**mapping)
 
 
 def dbg_safestring(string):
