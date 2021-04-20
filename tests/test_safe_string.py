1126e705fac5c4739abf48541f632df824ce2010
disconsis <ketan.kanishka@nyu.edu>
2021-04-20 17:52:04 +0530

    fix tests to be compatible with new safe_string

--- tests/test_safe_string.py
+++ tests/test_safe_string.py
@@ -26,12 +26,13 @@ def gen_random_safe_from_unsafe(string):
 def test_can_create_new_safe_strings():
     for length in range(100):
         unsafe = gen_random_string(length)
-        safe_string(unsafe)
+        trusted = gen_random_trusted(length)
+        safe_string(unsafe, trusted)
 
 
 def test_default_trusted_is_same_length_as_string():
     safe = gen_random_safe_string(100)
-    assert len(safe._trusted) == len(safe.string)
+    assert len(safe._trusted) == len(safe)
 
 
 def test_can_initialize_with_custom_trusted():
@@ -39,30 +40,32 @@ def test_can_initialize_with_custom_trusted():
 
 
 def test_safe_strings_return_correct_values_for_string_to_non_string_functions():
-    s1 = gen_random_safe_string(18)
-    s2 = gen_random_safe_string(20)
+    u1 = gen_random_string(18)
+    u2 = gen_random_string(20)
+    s1 = gen_random_safe_from_unsafe(u1)
+    s2 = gen_random_safe_from_unsafe(u2)
 
-    assert len(safe_string(s1)) == len(s1)
-    assert len(safe_string(s2)) == len(s2)
+    assert len(s1) == len(u1)
+    assert len(s2) == len(u2)
 
-    assert (s1 == s2) == (safe_string(s1) == safe_string(s2))
-    assert (s1 < s2) == (safe_string(s1) < safe_string(s2))
-    assert (s1 > s2) == (safe_string(s1) > safe_string(s2))
-    assert (s1 <= s2) == (safe_string(s1) <= safe_string(s2))
-    assert (s1 >= s2) == (safe_string(s1) >= safe_string(s2))
+    assert (s1 == s2) == (u1 == u2)
+    assert (s1 < s2) == (u1 < u2)
+    assert (s1 > s2) == (u1 > u2)
+    assert (s1 <= s2) == (u1 <= u2)
+    assert (s1 >= s2) == (u1 >= u2)
 
-    assert s1.isascii() == safe_string(s1).isascii()
-    assert s1.islower() == safe_string(s1).islower()
-    assert s1.isupper() == safe_string(s1).isupper()
-    assert s1.istitle() == safe_string(s1).istitle()
-    assert s1.isspace() == safe_string(s1).isspace()
-    assert s1.isdecimal() == safe_string(s1).isdecimal()
-    assert s1.isdigit() == safe_string(s1).isdigit()
-    assert s1.isnumeric() == safe_string(s1).isnumeric()
-    assert s1.isalpha() == safe_string(s1).isalpha()
-    assert s1.isalnum() == safe_string(s1).isalnum()
-    assert s1.isidentifier() == safe_string(s1).isidentifier()
-    assert s1.isprintable() == safe_string(s1).isprintable()
+    assert s1.isascii() == u1.isascii()
+    assert s1.islower() == u1.islower()
+    assert s1.isupper() == u1.isupper()
+    assert s1.istitle() == u1.istitle()
+    assert s1.isspace() == u1.isspace()
+    assert s1.isdecimal() == u1.isdecimal()
+    assert s1.isdigit() == u1.isdigit()
+    assert s1.isnumeric() == u1.isnumeric()
+    assert s1.isalpha() == u1.isalpha()
+    assert s1.isalnum() == u1.isalnum()
+    assert s1.isidentifier() == u1.isidentifier()
+    assert s1.isprintable() == u1.isprintable()
 
 
 def test_string_to_same_length_string_functions_dont_change_trusted():
@@ -83,22 +86,22 @@ def test_substring_finding_functions_work_correctly():
     start = "abcde"
     end = "oo"
 
-    assert (needle1 in haystack) == (safe_string(needle1) in safe_string(haystack))
-    assert (needle2 in haystack) == (safe_string(needle2) in safe_string(haystack))
+    safe_haystack = gen_random_safe_from_unsafe(haystack)
+    safe_needle1 = gen_random_safe_from_unsafe(needle1)
+    safe_needle2 = gen_random_safe_from_unsafe(needle2)
+    safe_start = gen_random_safe_from_unsafe(start)
+    safe_end = gen_random_safe_from_unsafe(end)
 
-    assert haystack.index(haystack[4:7]) == safe_string(haystack).index(safe_string(haystack[4:7]))
-    assert haystack.rindex(haystack[4:7]) == safe_string(haystack).rindex(safe_string(haystack[4:7]))
+    assert (needle1 in haystack) == (safe_needle1 in safe_haystack)
+    assert (needle2 in haystack) == (safe_needle2 in safe_haystack)
 
-    assert haystack.startswith(start) == safe_string(haystack).startswith(
-        safe_string(start)
-    )
-    assert haystack.startswith(end) == safe_string(haystack).startswith(
-        safe_string(end)
-    )
-    assert haystack.endswith(start) == safe_string(haystack).endswith(
-        safe_string(start)
-    )
-    assert haystack.endswith(end) == safe_string(haystack).endswith(safe_string(end))
+    assert haystack.index(haystack[4:7]) == safe_haystack.index(safe_haystack[4:7])
+    assert haystack.rindex(haystack[4:7]) == safe_haystack.rindex(safe_haystack[4:7])
+
+    assert haystack.startswith(start) == safe_haystack.startswith(safe_start)
+    assert haystack.startswith(end) == safe_haystack.startswith(safe_end)
+    assert haystack.endswith(start) == safe_haystack.endswith(safe_start)
+    assert haystack.endswith(end) == safe_haystack.endswith(safe_end)
 
 
 def test_substrings_are_returned_correctly():
@@ -110,7 +113,7 @@ def test_substrings_are_returned_correctly():
     for i in range(-(length + 5), (length + 5)):
         for j in range(-(length + 5), (length + 5)):
             sub = safe[i:j]
-            assert sub.string == string[i:j]
+            assert sub == string[i:j]
             assert sub._trusted == trusted[i:j]
 
 
@@ -118,7 +121,7 @@ def test_safe_string_format():
     template = gen_random_safe_from_unsafe("my name is {name} the {0}nd")
     name = gen_random_safe_from_unsafe("ramses")
     formatted = template.format(2, name=name)
-    assert formatted.string == "my name is ramses the 2nd"
+    assert formatted == "my name is ramses the 2nd"
     assert formatted._trusted == (
         template._trusted[: len("my name is ")]
         + name._trusted
