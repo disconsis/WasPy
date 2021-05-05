434e9c2850a409826ee62052c4b36e95ad9bdcfc
agnithamohan <agnitha.mohan@gmail.com>
2021-05-05 02:14:09 -0400

    added webapp

---
+++ webapp/app.py
@@ -0,0 +1,37 @@
+"""basic Flask app - demo of using a variable in a route"""
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '30lTgkRcQ9'
+Bootstrap(app)
+
+class NameForm(FlaskForm):
+    name = StringField('Enter your name:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/demo', methods=['GET','POST'])
+def demo():
+    form = NameForm() 
+    message = ""
+    query = 'SELECT * FROM users WHERE name = '
+
+    if form.validate_on_submit():
+        name = form.name.data
+        query += "'" + name + "'"
+        if has_sqli(query):
+            # empty the form field
+            form.name.data = ""
+            message = "SQLI has been detected."
+        else:
+            message = "SQLI has not been detected/there's a syntax error."
+    return render_template('demo.html', form=form, message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
