39700750b9d407dff8041d284b9a505d22e6095c
disconsis <ketan.kanishka@nyu.edu>
2021-05-13 03:51:31 +0530

    add false positive evaluation

---
+++ evaluation/eval.py
@@ -0,0 +1,41 @@
+import csv
+from pprint import pprint
+import sys
+import os.path
+
+os.chdir(os.path.dirname(__file__))
+
+sys.path.append("../safe_string")
+from safe_string import safe_string
+from sql import sqli, is_valid_sql
+
+
+with open("fake_people.csv", newline="") as fp:
+    reader = csv.DictReader(fp)
+
+    print("FALSE POSITIVES")
+    print("===============")
+
+    fp = 0
+    tot = 0
+    rows = 0
+    try:
+        for template in ["SELECT * FROM people WHERE field = '{}'",
+                         'SELECT * FROM people WHERE field = "{}"']:
+            template = safe_string._new_trusted(template)
+            for row in reader:
+                rows += 1
+                for value in row.values():
+                    query = template.format(value)
+                    if is_valid_sql(query):
+                        tot += 1
+                        if sqli(query):
+                            print(query)
+                            fp += 1
+
+                if rows >= 100:
+                    raise StopIteration
+    except StopIteration:
+        pass
+    finally:
+        print(f"False positives: {fp}/{tot}")
