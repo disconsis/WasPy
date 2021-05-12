a84c73311ab771a6ddb4a1f7973fb759a1c231ef
agnithamohan <agnitha.mohan@gmail.com>
2021-05-12 16:01:41 -0400

    changes to count false neg

--- sample_source/test.py
+++ sample_source/test.py
@@ -1,19 +1,26 @@
 
 import csv
+import sql
 # git clone https://github.com/omurugur/SQL_Injection_Payload.git at the same level as WasPy
 f = open('../../SQL_Injection_Payload/SQL-Inj-Payload.txt','r')
 fd = open('result.csv','a') 
 fnames = ['query', 'has_sqli']
 writer = csv.DictWriter(fd, fieldnames=fnames)
 writer.writeheader()
+fn = 0
+tot = 0
 for x in f:
     for template in ['SELECT * FROM users WHERE name = ', 'SELECT * FROM users WHERE id = ']:
+        tot += 1
         value = x
         template += "'" + value + "'"
+        if not sql.sqli(template) and sql.is_valid_sql(template):
+            fn +=1
 
-        result = "True" if has_sqli(template) else "False"
-        writer.writerow({'query' : str(template), 'has_sqli': result})
+        # result = "True" if has_sqli(template) else "False"
+        # writer.writerow({'query' : str(template), 'has_sqli': result})
         
+print("False Negatives:", fn)
 print("DONE")
 
 
