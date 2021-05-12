43a1eb06a312ed93188fdd42cbf36fe3928156bd
disconsis <ketan.kanishka@nyu.edu>
2021-05-12 08:16:02 +0530

    refactor

--- safe_string/safe_execute.py
+++ safe_string/safe_execute.py
@@ -1,60 +1,30 @@
-"""
-
-sqlite3
-
-override sqlite3.Connection.execute()
-
-"""
-
 from forbiddenfruit import curse
 import sqlite3
+import psycopg2
+from functools import wraps
+
 import sql
-import psycopg2 
 
 
-sqlite3_unsafe_execute = sqlite3.Connection.execute
-sqlite3_unsafe_executemany = sqlite3.Connection.executemany
-psycopg2_unsafe_execute = psycopg2.extensions.cursor.execute
-psycopg2_unsafe_executemany = psycopg2.extensions.cursor.executemany 
+unsafe_execute_classes = [
+    sqlite3.Connection,
+    psycopg2.extensions.cursor,
+]
 
-def psycopg2_safe_execute(connection, query, *args, **kwargs):
-    # print(f"sqlite3 -- query: {query!r} args: {args!r}")
-    if sql.sqli(query):
-        print("[!] SQLi detected in psycopg2")
-        print(f"query: {query!r}")
-        return
+for class_ in unsafe_execute_classes:
+    for func_name in ("execute", "executemany"):
+        try:
+            unsafe_func = getattr(class_, func_name)
+        except AttributeError:
+            pass
+        else:
+            @wraps(unsafe_func)
+            def safe_func(self, query, *args, **kwargs):
+                if sql.sqli(query):
+                    print("[!] SQLi detected in psycopg2")
+                    print(f"query: {query!r}")
+                    return
 
-    return psycopg2_unsafe_execute(connection, query, *args, **kwargs)
+                return unsafe_func(self, query, *args, **kwargs)
 
-def psycopg2_safe_executemany(connection, query, *args, **kwargs):
-    # print(f"sqlite3 -- query: {query!r} args: {args!r}")
-    if sql.sqli(query):
-        print("[!] SQLi detected in psycopg2")
-        print(f"query: {query!r}")
-        return
-
-    return psycopg2_unsafe_executemany(connection, query, *args, **kwargs)
-
-def sqlite3_safe_execute(connection, query, *args):
-    # print(f"sqlite3 -- query: {query!r} args: {args!r}")
-    if sql.sqli(query):
-        print("[!] SQLi detected in sqlite3")
-        print(f"query: {query!r}")
-        return
-
-    return sqlite3_unsafe_execute(connection, query, *args)
-
-def sqlite3_safe_executemany(connection, query, *args):
-    # print(f"sqlite3 -- query: {query!r} args: {args!r}")
-    if sql.sqli(query):
-        print("[!] SQLi detected in sqlite3")
-        print(f"query: {query!r}")
-        return
-
-    return sqlite3_unsafe_executemany(connection, query, *args)
-
-
-curse(sqlite3.Connection, "execute", sqlite3_safe_execute)
-curse(sqlite3.Connection, "executemany", sqlite3_safe_executemany)
-curse(psycopg2.extensions.cursor, "execute", psycopg2_safe_execute)
-curse(psycopg2.extensions.cursor, "executemany", psycopg2_safe_executemany)
+            curse(class_, func_name, safe_func)
