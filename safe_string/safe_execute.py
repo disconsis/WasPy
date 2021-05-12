e20fca99cf781452855f3d6e44b087d6acfe1ae6
disconsis <ketan.kanishka@nyu.edu>
2021-05-12 10:09:24 +0530

    return proper error classes on detecting sqli

--- safe_string/safe_execute.py
+++ safe_string/safe_execute.py
@@ -9,15 +9,14 @@ import sql
 __completed = False
 
 
-def wrap(class_, unsafe_func):
+def wrap(class_, unsafe_func, error_class):
     @wraps(unsafe_func)
     def safe_func(self, query, *args, **kwargs):
         # print(f'DEBUG: calling {class_.__module__}.{unsafe_func.__name__}')
         if sql.sqli(query):
             print("[!] SQLi detected in {}".format(class_.__module__))
             print(f"query: {query!r}")
-            # FIXME: replace with the module's error type
-            raise RuntimeError("sqli detected")
+            raise error_class("sqli detected")
 
         return unsafe_func(self, query, *args, **kwargs)
 
@@ -26,20 +25,20 @@ def wrap(class_, unsafe_func):
 
 if not __completed:
     unsafe_execute_classes = [
-        sqlite3.Connection,
-        sqlite3.Cursor,
-        psycopg2.extensions.cursor,
-        mysql.connector.cursor_cext.CMySQLCursor,
+        (sqlite3.Connection, sqlite3.Error),
+        (sqlite3.Cursor, sqlite3.Error),
+        (psycopg2.extensions.cursor, psycopg2.OperationalError),
+        (mysql.connector.cursor_cext.CMySQLCursor, mysql.connector.Error),
     ]
 
-    for class_ in unsafe_execute_classes:
+    for class_, error_class in unsafe_execute_classes:
         for func_name in ("execute", "executemany"):
             try:
                 unsafe_func = getattr(class_, func_name)
             except AttributeError:
                 pass
             else:
-                safe_func = wrap(class_, unsafe_func)
+                safe_func = wrap(class_, unsafe_func, error_class)
                 curse(class_, func_name, safe_func)
 
     __completed = True
