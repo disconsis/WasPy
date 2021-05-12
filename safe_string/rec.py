5a7dea644e9b5288f4f2518ba323d0065ee38cf3
disconsis <ketan.kanishka@nyu.edu>
2021-05-13 02:00:45 +0530

    handle out paths in rec.py

--- safe_string/rec.py
+++ safe_string/rec.py
@@ -1,14 +1,28 @@
 import glob 
-import sys
+import os
+from argparse import ArgumentParser
 from parsing import replace_safe_str
+import shutil
 
-my_path = '../' 
-if len(sys.argv) > 1: 
-    my_path = sys.argv[1]
+parser = ArgumentParser()
+parser.add_argument("in_path")
+parser.add_argument("--out", dest="out_path")
+args = parser.parse_args()
 
-files = glob.glob(my_path + '/**/*.py', recursive=True)
-for file in files:
-    print(file)
+if args.out_path:
+    if os.path.isdir(args.in_path):
+        shutil.copytree(args.in_path, args.out_path)
+    else:
+        shutil.copyfile(args.in_path, args.out_path)
+
+    path = args.out_path
+
+else:
+    path = args.in_path
+
+
+def replace_file(file):
+    print("Writing:", file)
     replaced_data = None
     with open(file, 'r') as fileptr:
         data = fileptr.read()
@@ -16,3 +30,12 @@ for file in files:
     if replaced_data is not None:
         with open(file, 'w') as fileptr:
             fileptr.write(replaced_data)
+
+
+if os.path.isdir(path):
+    files = glob.glob(path + '/**/*.py', recursive=True)
+    for file in files:
+        replace_file(file)
+else:
+    replace_file(path)
+
